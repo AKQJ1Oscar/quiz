23ed466fa03834818cbd17cfdd8ecec90329a92d
AKQJ1Oscar <oscarvb94@gmail.com>
2016-04-26 19:55:02 +0200

    primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+/* GET home page */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz', url: 'https://github.com/AKQJ1Oscar/quiz' });
 });
 
+/* GET author page */
 router.get('/author', function(req, res, next) {
   res.render('author', { title: 'Créditos', autor: 'Óscar Vázquez Babón', foto: 'https://avatars3.githubusercontent.com/u/16836716', url: 'https://github.com/AKQJ1Oscar/quiz' });
 });
+
+/* GET question */
+router.get('/question', quizController.question);
+
+/* GET check */
+router.get('/check', quizController.check);
 
 module.exports = router;
