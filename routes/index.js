0243eb6d7675827bf63e922ccb77f53be7c03b62
AKQJ1Oscar <oscarvb94@gmail.com>
2016-04-26 14:23:47 +0200

    footer y página de creditos

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', url: 'https://github.com/AKQJ1Oscar/quiz' });
+});
+
+router.get('/author', function(req, res, next) {
+  res.render('author', { title: 'Créditos', autor: 'Óscar Vázquez Babón', foto: 'https://avatars3.githubusercontent.com/u/16836716', url: 'https://github.com/AKQJ1Oscar/quiz' });
 });
 
 module.exports = router;
